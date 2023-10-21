8d35d587d362c7c4b4b7b7e2ee460ee8a8a05d48
Bryan <bryan.riveracv@gmail.com>
2023-10-21 15:36:53 +0200

    Segundo ej

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(wordToRepeat, num) {
+    if (num<0){
+        return 'ERROR';
+    }else {
+    let outputMsg = '';
+    for (let i = 0; i < num; i++){
+        outputMsg += wordToRepeat;
+    }
+    return outputMsg;
+    }
 };
+console.log(repeatString('ja',1));
 
 // Do not edit below this line
 module.exports = repeatString;
