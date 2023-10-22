aaf8e59fd7f434a0256435a1131d22be35f50a91
Bryan <bryan.riveracv@gmail.com>
2023-10-22 14:54:32 +0200

    Noveno ej

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^a-z]/g, '');
 
+    return str === str.split('').reverse().join('');
 };
 
+console.log(palindromes('a nut for a jar of tuna'));
 // Do not edit below this line
 module.exports = palindromes;
