336ffa6ad0c068ea1c58504e8935203754f6cbbd
Bryan <bryan.riveracv@gmail.com>
2023-10-22 15:34:27 +0200

    Decimoprimer ej

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,24 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.map(book => book.title);
 };
+
+const books = [
+    {
+        title: 'Ariel',
+        author: 'Sylvia Plath'
+    },
+    {
+        title: 'El llano en llamas',
+        author: 'Juan Rulfo'
+    },
+    {
+        title: 'Bestiario',
+        author: 'H.P Lovecraft'
+    }
+];
+
+const titles = getTheTitles(books);
+console.log(titles);
 
 // Do not edit below this line
 module.exports = getTheTitles;
